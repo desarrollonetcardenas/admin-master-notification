f60448c25fad0dc28ecdc22f0fef5c6bc23dae12
http://hector.cardenas@gitlab.coppel.io <hector.cardenas>
2020-04-12 17:05:43 -0600

    More inputs added

--- src/app/notification/nueva-notificacion/nueva-notificacion.component.ts
+++ src/app/notification/nueva-notificacion/nueva-notificacion.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 export class NuevaNotificacionComponent implements OnInit {
 
   success: boolean;
+  IsContinuos: boolean;
 
-  constructor() { }
+  constructor() { 
+    this.IsContinuos = true;
+  }
 
   ngOnInit(): void {
   }
